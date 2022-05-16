fba0b1fa6b781af3bd792772038f61f19d32ad1b
Brandon Trautmann <btrautmann@users.noreply.github.com>
2022-05-16 13:22:11 -0400

    Allow for development deployment

--- simple_sync/lib/main_development.dart
+++ simple_sync/lib/main_development.dart
@@ -5,9 +5,16 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:simple_sync/app/app.dart';
 import 'package:simple_sync/bootstrap.dart';
+import 'package:simple_sync/firebase_options.dart';
 
-void main() {
-  bootstrap(() => const App());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await bootstrap(() => const App());
 }
